4b859faca389238a90b2f0615fdb3918571045e4
Edson Junior Casa <edy_sinho@msn.com>
2020-01-31 20:53:31 -0300

    Criado um verificador de idade

---
+++ Exercicio2/assets/js/javascript.js
@@ -0,0 +1,56 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[erro] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano -  Number(fano.value)
+        var genero = ' '
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        //Checa qual radio esta marcado.
+        if (fsex[0].checked) {
+            genero = 'Homen'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src','assets/img/img-menino.png')
+
+            }else if (idade < 21 ) {
+                //JOvem
+                img.setAttribute('src','assets/img/img-jovem-m.png')
+                
+            }else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src','assets/img/img-homem.png')
+            } else {
+                //idoso
+                img.setAttribute('src','assets/img/img-idoso-m.png')
+            }
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança f
+                img.setAttribute('src','assets/img/img-menina.png')
+
+            }else if (idade < 21 ) {
+                //JOvem f
+                img.setAttribute('src','assets/img/img-jovem-f.png')
+                
+            }else if (idade < 50) {
+                //Adulto f
+                img.setAttribute('src','assets/img/img-mulher.png')
+                
+            } else {
+                //idoso f
+                img.setAttribute('src','assets/img/img-idoso-f.png')
+                
+            }
+        }
+        res.style.textAlign = 'center'// Centraliza o testo via JavaScript
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
--- README.md
+++ README.md
@@ -12,3 +12,17 @@ Criado manipulação de algumas tags HTML de acordo com a hora do dia, modifican
 Finalizado o Exercicio 1.
 
 Foi adicionado minutos e Texto de Bom Dia, Boa Tarde e Boa Noite, melhorias feita. 
+
+----------------------------------------------------------------------------------------
+
+Exercicio 2 
+
+Verificador de idade
+
+1º Verifica a idade de acordo com a data digitada
+
+2º Verifica o sexo se é homem ou Mulher de acordo com o radio marcado.
+
+3º inseri uma imagem de acordo com a idade.
+
+----------------------------------------------------------------------------------------
