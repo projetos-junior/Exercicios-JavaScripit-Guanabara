d34ba871847277cac1361ca2d24bbf005793afc9
Edson Junior Casa <edy_sinho@msn.com>
2020-02-03 21:48:22 -0300

    Organização de codigos

--- Exercicio1/assets/js/javascript.js
+++ Exercicio1/assets/js/javascript.js
@@ -1,20 +1,21 @@
-function carregar(){
+function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagen')
     var data = new Date()
     var hora = data.getHours()
     var minuto = data.getMinutes()
-        if(hora >= 0 && hora <12){
+
+        if (hora >= 0 && hora <12) {
         //Bom Dia
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Bom dia`
         document.body.style.background = '#e0dfcb'
         img.src = 'assets/img/imgmanha.png'
-    }else if (hora >= 12 && hora <= 18){
+    }else if (hora >= 12 && hora <= 18) {
         //Boa Tarde
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Boa Tarde`
         document.body.style.background = '#866e38'
         img.src = 'assets/img/imgtarde.png'
-    }else{
+    }else {
         //Boa Noite
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Boa Noite`
         document.body.style.background = '#501800'
