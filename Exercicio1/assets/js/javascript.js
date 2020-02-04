3d3c0f8dcd79829466f0b416d54e711df8d56b53
Edson Junior Casa <edy_sinho@msn.com>
2020-02-03 22:04:00 -0300

    Identasão de codigo

--- Exercicio1/assets/js/javascript.js
+++ Exercicio1/assets/js/javascript.js
@@ -5,17 +5,17 @@ function carregar() {
     var hora = data.getHours()
     var minuto = data.getMinutes()
 
-        if (hora >= 0 && hora <12) {
+    if (hora >= 0 && hora < 12) {
         //Bom Dia
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Bom dia`
         document.body.style.background = '#e0dfcb'
         img.src = 'assets/img/imgmanha.png'
-    }else if (hora >= 12 && hora <= 18) {
+    } else if (hora >= 12 && hora <= 18) {
         //Boa Tarde
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Boa Tarde`
         document.body.style.background = '#866e38'
         img.src = 'assets/img/imgtarde.png'
-    }else {
+    } else {
         //Boa Noite
         msg.innerHTML = `Agora são ${hora}:${minuto} Horas - Boa Noite`
         document.body.style.background = '#501800'
--- Exercicio2/assets/js/javascript.js
+++ Exercicio2/assets/js/javascript.js
@@ -1,55 +1,56 @@
-function verificar(){
+function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+    
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[erro] Verifique os dados e tente novamente')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano -  Number(fano.value)
+        var idade = ano - Number(fano.value)
         var genero = ' '
-        var img = document.createElement('img')// cria o elemento img
+        var img = document.createElement('img') // cria o elemento img
         img.setAttribute('id', 'foto') //atribui o ID no elemento img
         //Checa qual radio esta marcado.
         if (fsex[0].checked) {
             genero = 'Homen'
-            if(idade >= 0 && idade < 10){
+            if (idade >= 0 && idade < 10) {
                 //criança
-                img.setAttribute('src','assets/img/img-menino.png')
+                img.setAttribute('src', 'assets/img/img-menino.png')
 
-            }else if (idade < 21 ) {
+            } else if (idade < 21) {
                 //JOvem
-                img.setAttribute('src','assets/img/img-jovem-m.png')
-                
-            }else if (idade < 50) {
+                img.setAttribute('src', 'assets/img/img-jovem-m.png')
+
+            } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src','assets/img/img-homem.png')
+                img.setAttribute('src', 'assets/img/img-homem.png')
             } else {
                 //idoso
-                img.setAttribute('src','assets/img/img-idoso-m.png')
+                img.setAttribute('src', 'assets/img/img-idoso-m.png')
             }
-        } else if(fsex[1].checked){
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
-            if(idade >= 0 && idade < 10){
+            if (idade >= 0 && idade < 10) {
                 //criança f
-                img.setAttribute('src','assets/img/img-menina.png')
+                img.setAttribute('src', 'assets/img/img-menina.png')
 
-            }else if (idade < 21 ) {
+            } else if (idade < 21) {
                 //JOvem f
-                img.setAttribute('src','assets/img/img-jovem-f.png')
-                
-            }else if (idade < 50) {
+                img.setAttribute('src', 'assets/img/img-jovem-f.png')
+
+            } else if (idade < 50) {
                 //Adulto f
-                img.setAttribute('src','assets/img/img-mulher.png')
-                
+                img.setAttribute('src', 'assets/img/img-mulher.png')
+
             } else {
                 //idoso f
-                img.setAttribute('src','assets/img/img-idoso-f.png')
-                
+                img.setAttribute('src', 'assets/img/img-idoso-f.png')
+
             }
         }
-        res.style.textAlign = 'center'// Centraliza o testo via JavaScript
+        res.style.textAlign = 'center' // Centraliza o testo via JavaScript
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.appendChild(img)
     }
